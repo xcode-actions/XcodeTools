30c62125d1a1c6ecd74b15249133b4e295f54fb1
Frizlab <fload@me.com>
2022-01-16 13:47:43 +0100

    Friendlify project for SPM

---
+++ Sources/XcodeProj/CoreDataModelArtifacts_PBXModel/PBXModel+CoreDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  PBXModel+CoreDataModel.swift
+//  
+//
+//  Created by François Lamboley on 2022/1/16.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+
---
+++ Sources/XcodeProj/CoreDataModelArtifacts_PBXModel/PBXSourcesBuildPhase+CoreDataProperties.swift
@@ -0,0 +1,21 @@
+//
+//  PBXSourcesBuildPhase+CoreDataProperties.swift
+//  
+//
+//  Created by François Lamboley on 2022/1/16.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+
+extension PBXSourcesBuildPhase {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<PBXSourcesBuildPhase> {
+        return NSFetchRequest<PBXSourcesBuildPhase>(entityName: "PBXSourcesBuildPhase")
+    }
+
+
+}
