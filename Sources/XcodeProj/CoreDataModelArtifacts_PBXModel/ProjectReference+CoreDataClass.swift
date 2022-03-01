13494449d338d40c30047f13a141fa619a75f9e1
Frizlab <fload@me.com>
2022-03-01 02:47:10 +0100

    Friendlify project for SPM

---
+++ Sources/XcodeProj/CoreDataModelArtifacts_PBXModel/ProjectReference+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  ProjectReference+CoreDataClass.swift
+//  
+//
+//  Created by François Lamboley on 01/03/2022.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(ProjectReference)
+public class ProjectReference: NSManagedObject {
+
+}
