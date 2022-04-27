45043d34bfba9eeae48795bd06a9e618ba36261f
Frizlab <fload@me.com>
2022-04-28 00:38:38 +0200

    Add new command to pbxproj command

---
+++ Sources/xct-pbxproj/Commands/ListTargets.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+import ArgumentParser
+import XcodeProj
+
+
+
+struct ListTargets : ParsableCommand {
+	
+	static var configuration = CommandConfiguration(
+		abstract: "List the targets of a pbxproj file."
+	)
+	
+	@OptionGroup
+	var xctPbxprojOptions: XctPbxproj.Options
+	
+	@Flag
+	var print0: Bool = false
+	
+	func run() throws {
+		XctPbxproj.bootstrap()
+//		let logger = XctPbxproj.logger
+		
+		
+		let xcodeproj = try XcodeProj(path: xctPbxprojOptions.pathToXcodeproj)
+		let context = xcodeproj.managedObjectContext
+		try context.performAndWait{
+			let names = try xcodeproj.pbxproj.rootObject.getTargets().map{ try $0.getName() }
+			for name in names {
+				print(name, terminator: print0 ? "\0" : "\n")
+			}
+		}
+	}
+	
+}
