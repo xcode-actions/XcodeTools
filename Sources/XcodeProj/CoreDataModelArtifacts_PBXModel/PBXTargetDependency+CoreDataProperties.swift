dbe2a48f34197ad9cdf3426bf43d1545f6fb605d
Frizlab <fload@me.com>
2022-12-03 13:12:07 +0100

    Friendlify project for SPM

---
+++ Sources/XcodeProj/CoreDataModelArtifacts_PBXModel/PBXTargetDependency+CoreDataProperties.swift
@@ -0,0 +1,29 @@
+//
+/*
+ * PBXTargetDependency+CoreDataProperties.swift
+ * 
+ *
+ * Created by François Lamboley on 2022/12/03.
+ * 
+ */
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+
+extension PBXTargetDependency {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<PBXTargetDependency> {
+        return NSFetchRequest<PBXTargetDependency>(entityName: "PBXTargetDependency")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var platformFilter: String?
+    @NSManaged public var productRef: XCSwiftPackageProductDependency?
+    @NSManaged public var target: PBXTarget?
+    @NSManaged public var target_: PBXTarget?
+    @NSManaged public var targetProxy: PBXContainerItemProxy?
+
+}
