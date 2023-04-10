9b625c93fafb684c5726a9facd9f0221e0407566
Frizlab <fload@me.com>
2023-04-10 17:50:06 +0200

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -59,14 +59,14 @@ let package = Package(
 	}(),
 	dependencies: {
 		var res = [Package.Dependency]()
-		res.append(.package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"))
-//		res.append(.package(url: "https://github.com/apple/swift-crypto.git", from: "1.1.6"))
-		res.append(.package(url: "https://github.com/apple/swift-log.git", from: "1.4.2"))
-		res.append(.package(url: "https://github.com/happn-app/XibLoc.git", from: "1.1.1"))
-		res.append(.package(url: "https://github.com/SDGGiesbrecht/swift-crypto.git", from: "0.10107.0")) /* Because we’re using SDGGiesbrecht’s fork of swift-package-manager, we also have to use his fork of swift-crypto. */
-		res.append(.package(url: "https://github.com/SDGGiesbrecht/swift-package-manager.git", from: "0.50700.2")) /* Apple does not semver SPM for whatever reason. */
-		res.append(.package(url: "https://github.com/xcode-actions/clt-logger.git", from: "0.4.0"))
-		res.append(.package(url: "https://github.com/xcode-actions/stream-reader.git", from: "3.4.0"))
+		res.append(.package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.2"))
+//		res.append(.package(url: "https://github.com/apple/swift-crypto.git", from: "2.4.0"))
+		res.append(.package(url: "https://github.com/apple/swift-log.git", from: "1.5.2"))
+		res.append(.package(url: "https://github.com/happn-app/XibLoc.git", from: "1.2.5"))
+		res.append(.package(url: "https://github.com/SDGGiesbrecht/swift-crypto.git", from: "0.20400.0")) /* Because we’re using SDGGiesbrecht’s fork of swift-package-manager, we also have to use his fork of swift-crypto. */
+		res.append(.package(url: "https://github.com/SDGGiesbrecht/swift-package-manager.git", from: "0.50800.0")) /* Apple does not semver SPM for whatever reason. */
+		res.append(.package(url: "https://github.com/xcode-actions/clt-logger.git", from: "0.5.1"))
+		res.append(.package(url: "https://github.com/xcode-actions/stream-reader.git", from: "3.5.0"))
 		res.append(.package(url: "https://github.com/xcode-actions/swift-signal-handling.git", from: "1.1.0"))
 #if !canImport(System)
 		res.append(.package(url: "https://github.com/apple/swift-system.git", from: "1.0.0"))
--- Sources/SPMProj/SPMProj.swift
+++ Sources/SPMProj/SPMProj.swift
@@ -8,14 +8,15 @@ import Workspace
 
 
 /* Some libSPM help:
- * https://github.com/apple/swift-package-manager/blob/751f0b2a00276be2c21c074f4b21d952eaabb93b/Examples/package-info/Sources/package-info/main.swift */
+ * https://github.com/apple/swift-package-manager/blob/swift-5.8-RELEASE/Examples/package-info/Sources/package-info/example.swift */
 public struct SPMProj {
 	
 	public let rootURL: URL
 	public let projectManifestURL: URL
 	
 	public var targets: [SPMTarget] {
-		packageGraph.reachableTargets.filter(packageGraph.isInRootPackages).map(SPMTarget.init)
+#warning("TODO: platform selection…")
+		return packageGraph.reachableTargets.filter{ packageGraph.isInRootPackages($0, satisfying: .init(platform: .macOS)) }.map(SPMTarget.init)
 	}
 	
 	public init(path: String? = nil) throws {
@@ -27,13 +28,13 @@ public struct SPMProj {
 		self.projectManifestURL = rootURL.appendingPathComponent("Package.swift")
 		
 		let tempDir = FileManager.default.temporaryDirectory.appendingPathComponent(UUID().uuidString)
-		let workspace = try Workspace(forRootPackage: AbsolutePath((workspaceRoot ?? tempDir).path))
+		let workspace = try Workspace(forRootPackage: AbsolutePath(validating: (workspaceRoot ?? tempDir).path))
 		
 		let observability = ObservabilitySystem{ scope, diag in
 			Conf.logger?.debug("Message from SPM: \(diag)")
 		}
 		
-		self.packageGraph = try workspace.loadPackageGraph(rootPath: AbsolutePath(rootURL.path), observabilityScope: observability.topScope)
+		self.packageGraph = try workspace.loadPackageGraph(rootPath: AbsolutePath(validating: rootURL.path), observabilityScope: observability.topScope)
 		guard packageGraph.rootPackages.count == 1 else {
 			throw Err.cannotLoadPackage(rootURL)
 		}
