764cefab39a0937e2348817cfeb1f4afc38e1466
Frizlab <fload@me.com>
2023-09-09 07:53:12 +0200

    Add UnwrapOrThrow as an XcodeProj dependency

--- Package.swift
+++ Package.swift
@@ -66,6 +66,7 @@ let package = Package(
 		res.append(.package(url: "https://github.com/apple/swift-argument-parser.git",         from: "1.2.2"))
 //		res.append(.package(url: "https://github.com/apple/swift-crypto.git",                  from: "2.4.0"))
 		res.append(.package(url: "https://github.com/apple/swift-log.git",                     from: "1.5.2"))
+		res.append(.package(url: "https://github.com/Frizlab/UnwrapOrThrow.git",               from: "1.0.1-rc"))
 		res.append(.package(url: "https://github.com/happn-app/XibLoc.git",                    from: "1.2.5"))
 		res.append(.package(url: "https://github.com/SDGGiesbrecht/swift-crypto.git",          from: "0.20400.0")) /* Because we’re using SDGGiesbrecht’s fork of swift-package-manager, we also have to use his fork of swift-crypto. */
 		res.append(.package(url: "https://github.com/SDGGiesbrecht/swift-package-manager.git", from: "0.50800.0")) /* Apple does not semver SPM for whatever reason. */
@@ -258,7 +259,8 @@ let package = Package(
 		/* ***************** */
 		res.append(.target(name: "XcodeProj", dependencies: {
 			var res = [Target.Dependency]()
-			res.append(.product(name: "Logging", package: "swift-log"))
+			res.append(.product(name: "Logging",       package: "swift-log"))
+			res.append(.product(name: "UnwrapOrThrow", package: "UnwrapOrThrow"))
 			res.append(.target(name: "SPMProj"))
 			res.append(.target(name: "Utils"))
 			return res
