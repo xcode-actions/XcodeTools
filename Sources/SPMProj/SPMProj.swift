c993bdd2349fc479e2c7fab41c0a882a6da979d1
Frizlab <fload@me.com>
2023-09-09 07:54:25 +0200

    Make SPMProj Hashable

--- Sources/SPMProj/SPMProj.swift
+++ Sources/SPMProj/SPMProj.swift
@@ -14,16 +14,17 @@ import Workspace
  * The `swift-package-manager` dependency might not succeed in loading the packages if the Swift’s compiler version on the system is not the same as the dependency’s version.
  * The reason for this is `swift-package-manager` has to call the compiler to “parse” the `Package.swift` file which describe the package.
  * If the output of the compiler changed between versions, the parsing output won’t be recognized.
- * E.g. when building XcodeTools w/ `swift-package-manager` version `0.50800.0` on macOS 14.0 Beta with Xcode 15.0 beta, the packages fail to load. */
+ * E.g. using XcodeTools built w/ `swift-package-manager` version `0.50800.0` on macOS 14.0 Beta with the Xcode 15.0 beta toolchain will not work (packages will fail to load). */
+
+/**
+ The description of an SPM project.
+ 
+ This struct is Hashable.
+ Two SPMProj are considered equal if their root and project manifest URLs are equal. */
 public struct SPMProj {
 	
 	public let rootURL: URL
 	public let projectManifestURL: URL
-	
-	public var targets: [SPMTarget] {
-#warning("TODO: platform selection…")
-		return packageGraph.reachableTargets.filter{ packageGraph.isInRootPackages($0, satisfying: .init(platform: .macOS)) }.map(SPMTarget.init)
-	}
 	
 	public init(path: String? = nil) throws {
 		try self.init(url: path.flatMap{ URL(fileURLWithPath: $0) })
@@ -46,9 +47,28 @@ public struct SPMProj {
 		}
 	}
 	
+	public var targets: [SPMTarget] {
+#warning("TODO: platform selection…")
+		return packageGraph.reachableTargets.filter{ packageGraph.isInRootPackages($0, satisfying: .init(platform: .macOS)) }.map(SPMTarget.init)
+	}
+	
 	internal var packageGraph: PackageGraph
 	internal var resolvedPackage: ResolvedPackage {
 		packageGraph.rootPackages.first!
+	}
+	
+}
+
+
+extension SPMProj : Hashable {
+	
+	public static func ==(_ lhs: SPMProj, _ rhs: SPMProj) -> Bool {
+		return lhs.rootURL == rhs.rootURL && lhs.projectManifestURL == rhs.projectManifestURL
+	}
+	
+	public func hash(into hasher: inout Hasher) {
+		hasher.combine(rootURL)
+		hasher.combine(projectManifestURL)
 	}
 	
 }
