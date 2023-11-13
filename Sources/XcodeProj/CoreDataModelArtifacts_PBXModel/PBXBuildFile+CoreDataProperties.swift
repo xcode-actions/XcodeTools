eef267464313f5ec90299d1bad95eb5c8bfd29d4
Frizlab <fload@me.com>
2023-11-13 10:04:19 +0100

    Friendlify project for SPM

---
+++ Sources/XcodeProj/CoreDataModelArtifacts_PBXModel/PBXBuildFile+CoreDataProperties.swift
@@ -0,0 +1,27 @@
+//
+/*
+ * PBXBuildFile+CoreDataProperties.swift
+ * 
+ *
+ * Created by François Lamboley on 2023/11/13.
+ * 
+ */
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+
+extension PBXBuildFile {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<PBXBuildFile> {
+        return NSFetchRequest<PBXBuildFile>(entityName: "PBXBuildFile")
+    }
+
+    @NSManaged public var rawSettings: [String: Any]?
+    @NSManaged public var fileRef: PBXFileElement?
+    @NSManaged public var i_buildPhase: PBXBuildPhase?
+    @NSManaged public var productRef: XCSwiftPackageProductDependency?
+
+}
---
+++ Sources/XcodeProj/CoreDataModelArtifacts_PBXModel/PBXShellScriptBuildPhase+CoreDataProperties.swift
@@ -0,0 +1,31 @@
+//
+/*
+ * PBXShellScriptBuildPhase+CoreDataProperties.swift
+ * 
+ *
+ * Created by François Lamboley on 2023/11/13.
+ * 
+ */
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+
+extension PBXShellScriptBuildPhase {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<PBXShellScriptBuildPhase> {
+        return NSFetchRequest<PBXShellScriptBuildPhase>(entityName: "PBXShellScriptBuildPhase")
+    }
+
+    @NSManaged public var alwaysOutOfDate: NSNumber?
+    @NSManaged public var inputFileListPaths: [String]?
+    @NSManaged public var inputPaths: [String]?
+    @NSManaged public var outputFileListPaths: [String]?
+    @NSManaged public var outputPaths: [String]?
+    @NSManaged public var shellPath: String?
+    @NSManaged public var shellScript: String?
+    @NSManaged public var showEnvVarsInLog: NSNumber?
+
+}
