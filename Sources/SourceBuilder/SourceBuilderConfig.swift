96fa884b4bd22e83a9756e4f254f0b0687572cef
Frizlab <fload@me.com>
2024-09-25 12:44:32 +0200

    Do not allow setting the TaskLocal wrapper anymore as this is not supported in Swift 6

--- Sources/SourceBuilder/SourceBuilderConfig.swift
+++ Sources/SourceBuilder/SourceBuilderConfig.swift
@@ -32,21 +32,7 @@ public enum SourceBuilderConfig {
 	@TaskLocal
 	public static var urlSession: URLSession = .shared
 	
-	/**
-	 Everything ``SourceBuilder`` logs will go through this logger.
-	 
-	 This property is wrapped in a `TaskLocal`, which means you can change it in a particular task using `$logger.withValue(...)`.
-	 
-	 If you use a `TaskLocal` logger in your app, you can assign the *wrapper* value of the logger using ``setLogger(wrappedLogger:)``. */
-	@TaskLocal
 	public static var logger: Logger? = .init(label: "com.xcode-actions.SourceBuilder")
-	/**
-	 Sets the *wrapper* of the logger directly.
-	 
-	 This allows synchronisation of your own `TaskLocal` logger with ``SourceBuilder``’s. */
-	public static func setLogger(wrappedLogger: TaskLocal<Logger?>) {
-		_logger = wrappedLogger
-	}
 	
 }
 
