af09520ba1b92a90204b8d7f883cf6ce5c32e549
Frizlab <fload@me.com>
2026-01-22 10:00:00 +0100

    Update stream-reader’s URL

--- Package.swift
+++ Package.swift
@@ -59,10 +59,10 @@ let package = Package(
 		res.append(.package(url: "https://github.com/Frizlab/CollectionConcurrencyKit.git",       from: "0.3.0"))
 		res.append(.package(url: "https://github.com/Frizlab/GlobalConfModule.git",               from: "0.4.0"))
 		res.append(.package(url: "https://github.com/Frizlab/SafeGlobal.git",                     from: "0.3.0"))
+		res.append(.package(url: "https://github.com/Frizlab/stream-reader.git",                  from: "3.5.0"))
 		res.append(.package(url: "https://github.com/Frizlab/UnwrapOrThrow.git",                  from: "1.0.1"))
 		res.append(.package(url: "https://github.com/Frizlab/XibLoc.git",                         from: "1.5.1"))
 		res.append(.package(url: "https://github.com/xcode-actions/clt-logger.git",               from: "1.0.0"))
-		res.append(.package(url: "https://github.com/xcode-actions/stream-reader.git",            from: "3.5.0"))
 		res.append(.package(url: "https://github.com/xcode-actions/swift-process-invocation.git", from: "1.3.0-beta.5"))
 		res.append(.package(url: "https://github.com/xcode-actions/swift-signal-handling.git",    from: "1.1.0"))
 		return res
